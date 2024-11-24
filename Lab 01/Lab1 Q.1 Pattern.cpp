7ac40d4c58a0552f5fc7aab75632857cd368e994
Abdul Hayy Khan <134889827+abdulhayykhan@users.noreply.github.com>
2024-11-25 00:20:22 +0500

    Add files via upload

---
+++ Lab 01/Lab1 Q.1 Pattern.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "************\n"
+			"**********\n"
+			"*****\n"
+			"**\n"
+			"Xx\n"
+			"Xxxxxxxx\n"
+			"Xxxxxxxxxxxx";
+				
+    return 0;
+}
---
+++ Lab 01/Lab1 Q.2 CV.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Personal Information
+    cout << "Name: Abdul Hayy Khan\n";  
+    cout << "Father's Name: Ihtiram Ulfat\n";  
+    cout << "CNIC: 42201-7926160-1\n";  
+    
+    // Academic Information
+    cout << "Qualification: Intermediate" << endl; 
+    cout << "Semester: 4" << endl;  
+    cout << "CGPA: 3.75" << endl;  
+
+    return 0;
+}
